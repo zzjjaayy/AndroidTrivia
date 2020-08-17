dc1d24565cd266b17c7e46095d46ebae2eb4c428
zzjjaayy <jayprimerathod@gmail.com>
2020-08-17 23:41:06 +0530

    OverFlow Menu: Added an overflow menu in the TitleFragment fragment and mapped the button to the aboutFragment. NOTE: The id of the menu item needs to be the same as the id of the destination fragment or activity.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,13 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -39,7 +38,20 @@ class TitleFragment : Fragment() {
         // THE SIMPLEST WAY TO WRITE THE ABOVE clickListener is -
         // binding.playButton.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
 
+        setHasOptionsMenu(true)
+        // turns on the menu in the fragment
+
         return binding.root
         // returning the root of the binding layout
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+        // inflating the menu
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController()) || super.onOptionsItemSelected(item)
     }
 }
