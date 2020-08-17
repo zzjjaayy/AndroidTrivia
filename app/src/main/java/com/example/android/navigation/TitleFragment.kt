91e71b3a3dbb6b20e5b01ba9ce948fe8eb721274
zzjjaayy <jayprimerathod@gmail.com>
2020-08-17 22:29:23 +0530

    Correction and 1st Navigation: Added Navigation Destinations in navigation.xml, Added click Listener for button to switch between fragments.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -24,6 +26,18 @@ class TitleFragment : Fragment() {
         // passing in the following parameters :
         // 1. inflater{passed in the onCreateView constructor}
         // 2. layout file, 3.container{parent Activity} , 4.false for not attaching the fragment to the parent activity
+
+        binding.playButton.setOnClickListener {view : View ->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            // findNavController is a function that returns the view's parent fragment's host controller
+            // in other words gives an instance of the navigation controller
+            // the id is of the action can be found in the properties if the arrow in the navigation graph
+            // the above line can also be written as -
+            // "view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)"
+        }
+
+        // THE SIMPLEST WAY TO WRITE THE ABOVE clickListener is -
+        // binding.playButton.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
 
         return binding.root
         // returning the root of the binding layout
